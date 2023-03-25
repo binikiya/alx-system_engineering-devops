3bf13fb6418c768ebf653d398c91e62c23e62c95
Biniyam <biniyamkiya3682@gmail.com>
2023-03-25 20:50:12 +0300

    Updated

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,5 @@
 # installing puppet-lint
+
 exec { 'puppet-lint':
-  command => /usr/bin/apt-get -y install puppet-lint -v '2.1.0',
+  command => '/usr/bin/apt-get -y install puppet-lint -v 2.1.0',
 }
